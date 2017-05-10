79b50caf1af4707cbf1738c6c63239e459cd8e82
cristhiangrojas <c-crisari@hotmail.com>
2017-05-09 19:06:35 -0500

    09052017

--- application/views/template/connect.php
+++ application/views/template/connect.php
@@ -4,137 +4,122 @@
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-md-8 col-lg-8 col-xl-8">
-
-						<!--<div>
-							 <div class="perfiles">
-								<img src="external/img/fondo.jpg" alt="foto de portada" class="foto_portada">
-								<img src="uploads/newsfeed/panel_centro/Logo_Vibra.png" alt="foto de perfil" class="foto_perfil">
-								<div class="background_perfil">
-								<i class="fa fa-comments fa-4x" aria-hidden="true" style="left: 5%;position: absolute;color: #55B3E5;"></i>
-								<i class="fa fa-plug fa-4x" aria-hidden="true" style="right: 3%;position: absolute;color: #55B3E5;"></i>
-								<div class="info_icon"><p style="left: 3%;position: absolute;">MESSAGE</p><p style="right: 3%;position: absolute;">CONNECT</p></div>
+						<div class="fondo_noticias">
+								<div class="search">
+								<h2>CONNECT WITH OTHERS</h2>
+									<div>
+										<div class="col-xs-6 col-sm-3 padding">
+											<span class="key_word">Key word</span>
+											<input type="text" class="input_key">
+										</div>
+										<div class="col-xs-6 col-sm-3 padding">
+											<button type="button" class="btn btn-default">Professional</button>
+										</div>
+										<div class="col-xs-6 col-sm-3 padding">
+											<button type="button" class="btn btn-default">Entrepreneur</button>
+										</div>
+										<div class="col-xs-6 col-sm-3 padding">
+											<button type="button" class="btn btn-default">Company / Organisation</button>
+										</div>
+									</div>
+									<div>
+										<div class="col-xs-6 col-sm-3 padding">
+										  <div class="btn-group btn-default">
+										    <button type="button" class="btn btn-default dropdown-toggle"
+										            data-toggle="dropdown">
+										      Location
+										      <i class="fa fa-chevron-down fa_down" aria-hidden="true"></i>
+										    </button>
+										    <ul class="dropdown-menu">
+										      <li><a href="#">Enlace #1</a></li>
+										      <li><a href="#">Enlace #2</a></li>
+										    </ul>
+										  </div>
+										</div>
+										<div class="col-xs-6 col-sm-3 padding">
+										  <div class="btn-group btn-default">
+										    <button type="button" class="btn btn-default dropdown-toggle"
+										            data-toggle="dropdown">
+										      Industry
+										      <i class="fa fa-chevron-down fa_down" aria-hidden="true"></i>
+										    </button>
+										    <ul class="dropdown-menu">
+										      <li><a href="#">Enlace #1</a></li>
+										      <li><a href="#">Enlace #2</a></li>
+										    </ul>
+										  </div>
+										</div>
+										<div class="col-xs-6 col-sm-3 padding">
+										  <div class="btn-group btn-default">
+										    <button type="button" class="btn btn-default dropdown-toggle"
+										            data-toggle="dropdown">
+										      Languages
+										      <i class="fa fa-chevron-down fa_down" aria-hidden="true"></i>
+										    </button>
+										    <ul class="dropdown-menu">
+										      <li><a href="#">Enlace #1</a></li>
+										      <li><a href="#">Enlace #2</a></li>
+										    </ul>
+										  </div>
+										</div>
+										<div class="col-xs-6 col-sm-3 padding">
+										  <div class="btn-group btn-default">
+										    <button type="button" class="btn btn-default dropdown-toggle"
+										            data-toggle="dropdown">
+										      Educational background
+										      <span class="fa fa-chevron-down"></span>
+										    </button>
+										    <ul class="dropdown-menu">
+										      <li><a href="#">Enlace #1</a></li>
+										      <li><a href="#">Enlace #2</a></li>
+										    </ul>
+										  </div>
+										</div>
+									</div>
+								</div>
+								<div class="row persona">
+								<div class="col-xs-6 col-sm-9 contenido">
+									<div class="col-xs-6 col-sm-3 centrado">
+										<img src="external/img/avatar2.jpg" alt="foto de perfil" class="foto_perfilp">
+									</div>
+									<div class="col-xs-6 col-sm-4 centrado">
+										<div class="floating-box2"><span class="nombre">Claudia Sepúlveda</span></div>
+										<div class="floating-box2"><span class="profesion">COMM & Marketing</span></div>
+									</div>
+									<div class="col-xs-6 col-sm-5">
+										<div class="floating-box2"><span class="tdtitle bold">Located In -</span> <span class="tdtitle">Australia</span></div>
+										<div class="floating-box2"><span class="tdtitle bold">Industry -</span><span class="tdtitle"> Marketing</span></div>
+										<div class="floating-box2"><span class="tdtitle bold">Languages -</span><span class="tdtitle"> English / Spanish</span></div>
+										<div class="floating-box2"><span class="tdtitle bold">Education background -</span><span class="tdtitle"> Master</span></div>
+									</div>
+								</div>
+								<div class="col-xs-6 col-sm-3 centrado">
+									<button type="button" class="btn btn-default btn_connect"><i class="logo">Connect</i>&nbsp;<i class="fa fa-plug logos"></i></button>
 								</div>
 							</div>
-							<br><br><br><br><br>
-
-							<div class="container">
-								<form class="form-inline" role="form">
-									<div class="form-group">
-									Claudia Sepúlveda
+							<hr class="hr">
+								<div class="row persona">
+								<div class="col-xs-6 col-sm-9 contenido">
+									<div class="col-xs-6 col-sm-3 centrado">
+										<img src="external/img/avatar2.jpg" alt="foto de perfil" class="foto_perfilp">
 									</div>
-									<div class="form-group">
-									<span class="help-block">Country of Origin</span>
-									Chile
+									<div class="col-xs-6 col-sm-4 centrado">
+										<div class="floating-box2"><span class="nombre">Claudia Sepúlveda</span></div>
+										<div class="floating-box2"><span class="profesion">COMM & Marketing</span></div>
 									</div>
-									<div class="form-group">
-									<span class="help-block">Country of current residency</span>
-									Australia
+									<div class="col-xs-6 col-sm-5">
+										<div class="floating-box2"><span class="tdtitle bold">Located In -</span> <span class="tdtitle">Australia</span></div>
+										<div class="floating-box2"><span class="tdtitle bold">Industry -</span><span class="tdtitle"> Marketing</span></div>
+										<div class="floating-box2"><span class="tdtitle bold">Languages -</span><span class="tdtitle"> English / Spanish</span></div>
+										<div class="floating-box2"><span class="tdtitle bold">Education background -</span><span class="tdtitle"> Master</span></div>
 									</div>
-									<div class="form-group">
-									<span class="help-block">Date of birth</span>
-									28 / Jun / 1988
-									</div>
-								</form>
+								</div>
+								<div class="col-xs-6 col-sm-3 centrado">
+									<button type="button" class="btn btn-default btn_connect"><i class="logo">Connect</i>&nbsp;<i class="fa fa-plug logos"></i></button>
+								</div>
 							</div>
-
-
-						</div> -->
-<div class="row">
-	<div class="col-xs-6 col-sm-12">
-		<img src="external/img/fondo.jpg" alt="foto de portada" class="foto_portada">
-		<img src="uploads/newsfeed/panel_centro/Logo_Vibra.png" alt="foto de perfil" class="foto_perfil">
-		<div class="background_perfil">
-		<i class="fa fa-comments fa-4x icono2" aria-hidden="true"></i>
-		<i class="fa fa-plug fa-4x icono1" aria-hidden="true"></i>
-		</div>
-	</div>
-</div>
-<br>
-<div class="row fondo_noticias">
-  <div class="col-xs-6 col-sm-3 padres"><p class="hijo">Claudia Sepúlveda</p></div>
-  <div class="col-xs-6 col-sm-3 padres"><span class="ayudas">Country of Origin</span><p class="datos">Chile</p></div>
-  <div class="col-xs-6 col-sm-3 padres"><span class="ayudas">Country of current residency</span><p class="datos">Australia</p></div>
-  <div class="col-xs-6 col-sm-3 padres"><span class="ayudas">Date of birth</span><p class="datos">28 / Jun / 1988</p></div>
-</div>
-<br>
-<div class="row">
-	<div class="col-xs-6 col-sm-4 fondo_noticias alejar" id="profesional_information">
-		<div class="floating-box">Proffesional Information</div>
-		<div class="floating-box2"><span class="tdtitle">Occupation:</span> COMM & Marketing</div>
-		<div class="floating-box2"><span class="tdtitle">Industry:</span> Advertising</div>
-		<div class="floating-box2"><span class="tdtitle">Education Background:</span> Master Degree</div>
-		<div class="floating-box2"><span class="tdtitle">Languages:</span> Spanish / English</div>
-	</div>
-	<div class="col-xs-6 col-sm-4 fondo_noticias alejar" id="skills">
-			<div class="floating-box">Skills</div>
-		<div class="floating-box3 tdtitle">Marketing</div>
-		<div class="floating-box3 tdtitle">Bookkeeping</div>
-		<div class="floating-box3 tdtitle">Marketing</div>
-		<div class="floating-box3 tdtitle">Spanish</div>
-		<div class="floating-box3 tdtitle">Systems</div>
-		<div class="floating-box3 tdtitle">Spanish</div>
-		<div class="floating-box3 tdtitle">Bookkeeping</div>
-		<div class="floating-box3 tdtitle">Mathematics</div>
-		<div class="floating-box3 tdtitle">Bookkeeping</div>
-		<div class="floating-box3 tdtitle">Systems</div>
-		<div class="floating-box3 tdtitle">Creativity</div>
-		<div class="floating-box3 tdtitle">Systems</div>
-		<div class="floating-box3 tdtitle">Mathematics</div>
-		<div class="floating-box3 tdtitle">Leadership</div>
-	</div>
-	<div class="col-xs-6 col-sm-3 fondo_noticias alejar">
-			<div class="floating-box">Interests</div>
-		<div class="floating-box4 tdtitle">Ingeniering</div>
-		<div class="floating-box4 tdtitle">Ingeniering</div>
-		<div class="floating-box4 tdtitle">Arts</div>
-		<div class="floating-box4 tdtitle">Arts</div>
-		<div class="floating-box4 tdtitle">Culture</div>
-		<div class="floating-box4 tdtitle">Culture</div>
-		<div class="floating-box4 tdtitle">Social</div>
-		<div class="floating-box4 tdtitle">Social</div>
-		<div class="floating-box4 tdtitle">Law</div>
-		<div class="floating-box4 tdtitle">Law</div>
-	</div>
-</div>
-<br>
-<div class="row fondo_noticias">
-	<div class="col-xs-2 col-sm-1">
-	<img src="external/img/BIO.png" alt="BIO" class="bio">
-	</div>
-	<div class="col-xs-2 col-sm-11 texto">
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	<br>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	<br>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</div>
-</div>
-<br>
-<center>
-<div class="btn-group">
-  <button type="button" class="btn btn-default"><i class="logo fa-2x">Linked</i>&nbsp;<i class="fa fa-linkedin-square fa-2x" style="color: #000;"></i></button>
-  <button type="button" class="btn btn-default"><i class="fa fa-facebook-official fa-2x" style="color: #000;"></i>&nbsp;<i class="logo fa-2x">facebook</i></button>
-  <button type="button" class="btn btn-default"><i class="logo fa-2x">twitter</i>&nbsp;<i class="fa fa-twitter fa-2x" style="color: #000;"></i></button>
-  <button type="button" class="btn btn-default"><i class="fa fa-instagram fa-2x" style="color: #000;"></i>&nbsp;<i class="logo fa-2x">Instagram</i></button>
-  <button type="button" class="btn btn-default"><i class="fa fa-chrome fa-2x" style="color: #000;"></i>&nbsp;<i class="logo fa-2x">http://</i></button>
-</div>
-</center>
-
+							<hr class="hr">
+						</div>
 					</div>
 					<div class="col-md-4 col-lg-4 col-xl-4">
 						<div class="fondo_sponsors">
