7a811c71f5ea20895f79724c12648a0c3fbca8f6
cristhiangrojas <c-crisari@hotmail.com>
2017-05-09 19:27:41 -0500

    Usuario miguel

---
+++ application/views/template/connect_user.php
@@ -0,0 +1,144 @@
+	<section id="newsfeed">
+
+		<div class="section_1">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-8 col-lg-8 col-xl-8">
+						<div class="row">
+							<div class="col-xs-6 col-sm-12">
+								<img src="external/img/fondo.jpg" alt="foto de portada" class="foto_portada">
+								<img src="external/img/avatar2.jpg" alt="foto de perfil" class="foto_perfil">
+								<div class="background_perfil">
+								<i class="fa fa-comments icono2" aria-hidden="true"></i>
+								<i class="fa fa-plug icono1" aria-hidden="true"></i>
+								</div>
+							</div>
+						</div>
+						<br>
+						<div class="row fondo_noticias">
+						  <div class="col-xs-6 col-sm-3 padres"><p class="hijo">Claudia Sepúlveda</p></div>
+						  <div class="col-xs-6 col-sm-3 padres"><span class="ayudas">Country of Origin</span><p class="datos">Chile</p></div>
+						  <div class="col-xs-6 col-sm-3 padres"><span class="ayudas">Country of current residency</span><p class="datos">Australia</p></div>
+						  <div class="col-xs-6 col-sm-3 padres"><span class="ayudas">Date of birth</span><p class="datos">28 / Jun / 1988</p></div>
+						</div>
+						<br>
+						<div class="row">
+							<div class="col-xs-6 col-sm-4 fondo_noticias alejar" id="profesional_information">
+								<div class="floating-box">Proffesional Information</div>
+								<div class="floating-box2"><span class="tdtitle">Occupation:</span> COMM & Marketing</div>
+								<div class="floating-box2"><span class="tdtitle">Industry:</span> Advertising</div>
+								<div class="floating-box2"><span class="tdtitle">Education Background:</span> Master Degree</div>
+								<div class="floating-box2"><span class="tdtitle">Languages:</span> Spanish / English</div>
+							</div>
+							<div class="col-xs-6 col-sm-4 fondo_noticias alejar" id="skills">
+									<div class="floating-box">Skills</div>
+								<div class="floating-box3 tdtitle">Marketing</div>
+								<div class="floating-box3 tdtitle">Bookkeeping</div>
+								<div class="floating-box3 tdtitle">Marketing</div>
+								<div class="floating-box3 tdtitle">Spanish</div>
+								<div class="floating-box3 tdtitle">Systems</div>
+								<div class="floating-box3 tdtitle">Spanish</div>
+								<div class="floating-box3 tdtitle">Bookkeeping</div>
+								<div class="floating-box3 tdtitle">Mathematics</div>
+								<div class="floating-box3 tdtitle">Bookkeeping</div>
+								<div class="floating-box3 tdtitle">Systems</div>
+								<div class="floating-box3 tdtitle">Creativity</div>
+								<div class="floating-box3 tdtitle">Systems</div>
+								<div class="floating-box3 tdtitle">Mathematics</div>
+								<div class="floating-box3 tdtitle">Leadership</div>
+							</div>
+							<div class="col-xs-6 col-sm-3 fondo_noticias alejar">
+									<div class="floating-box">Interests</div>
+								<div class="floating-box4 tdtitle">Ingeniering</div>
+								<div class="floating-box4 tdtitle">Ingeniering</div>
+								<div class="floating-box4 tdtitle">Arts</div>
+								<div class="floating-box4 tdtitle">Arts</div>
+								<div class="floating-box4 tdtitle">Culture</div>
+								<div class="floating-box4 tdtitle">Culture</div>
+								<div class="floating-box4 tdtitle">Social</div>
+								<div class="floating-box4 tdtitle">Social</div>
+								<div class="floating-box4 tdtitle">Law</div>
+								<div class="floating-box4 tdtitle">Law</div>
+							</div>
+						</div>
+						<br>
+						<div class="row fondo_noticias">
+							<div class="col-xs-2 col-sm-1">
+							<img src="external/img/BIO.png" alt="BIO" class="bio">
+							</div>
+							<div class="col-xs-2 col-sm-11 texto">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							<br>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							<br>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</div>
+						</div>
+						<br>
+						<center>
+						<div class="btn-group">
+						  <button type="button" class="btn btn-default"><i class="logo">Linked</i>&nbsp;<i class="fa fa-linkedin-square logos"></i></button>
+						  <button type="button" class="btn btn-default"><i class="fa fa-facebook-official logos"></i>&nbsp;<i class="logo">facebook</i></button>
+						  <button type="button" class="btn btn-default"><i class="logo">twitter</i>&nbsp;<i class="fa fa-twitter logos"></i></button>
+						  <button type="button" class="btn btn-default"><i class="fa fa-instagram logos"></i>&nbsp;<i class="logo">Instagram</i></button>
+						  <button type="button" class="btn btn-default"><i class="fa fa-chrome logos"></i>&nbsp;<i class="logo">http://</i></button>
+						</div>
+						</center>
+
+					</div>
+					<div class="col-md-4 col-lg-4 col-xl-4">
+						<div class="fondo_sponsors">
+							<h6>Sponsors</h6>
+							<!--<img src="<?php echo base_url() ?>external/img/sponsors.jpg" alt="" class="img-responsive" style="width: 100%;">
+							<img src="<?php echo base_url() ?>external/img/sponsors1.jpg" alt="" class="img-responsive" style="width: 100%;">!
+							<?php 
+
+				                if ( count($listado_panel_centro_sponsors) > 0 ) 
+				                {
+				                  foreach ($listado_panel_centro_sponsors as $row)
+				                  {
+				                    echo '<img src="'.base_url().'uploads/newsfeed/panel_derecha/sponsors/'.$row->imagen.'" class="img-responsive" style="width: 100%;" />';
+				                  }
+				                }else{
+				                  echo "";
+				                }
+
+				              ?>
+						</div><br />
+						<div class="fondo_sponsors">
+							<h6>Connect</h6>
+							<!--<img src="<?php echo base_url() ?>external/img/connect.jpg" alt="" class="img-responsive">!-->
+							<?php 
+
+				                if ( count($listado_panel_centro_connect) > 0 ) 
+				                {
+				                  foreach ($listado_panel_centro_connect as $row)
+				                  {
+				                    echo '<img src="'.base_url().'uploads/newsfeed/panel_derecha/connect/'.$row->imagen.'" class="img-responsive" style="width: 100%;" />';
+				                  }
+				                }else{
+				                  echo "";
+				                }
+
+				              ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
